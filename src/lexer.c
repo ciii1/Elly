9fa823fb14c3da77402caf8f4fe1af7eb2ef1191
blend-smile <blend-smile@protonmail.com>
2021-08-17 20:32:18 +0700

    add int & float and fix bug

--- src/lexer.c
+++ src/lexer.c
@@ -2,34 +2,31 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include "include/compiler.h"
 #include "include/utils.h"
 
-char NEXT() {
-	char ch;
-	if((ch = fgetc(SOURCE)) != EOF) {
-		if (ch == '\n') {
-			LINE++;
-		}
-		return ch;
-	}
-	return EOF;
-}
-
 char PEEK() {
 	char ch;
-	if((ch = fgetc(SOURCE)) != EOF) {
-		ungetc(ch, SOURCE);
-		return ch;
+	ch = fgetc(SOURCE);
+	ungetc(ch, SOURCE);
+	return ch;
+}
+
+char NEXT() {
+	char ch;
+	ch = fgetc(SOURCE);
+	if (ch == '\n') {
+		LINE++;
 	}
-	return EOF;
+	return PEEK();
 }
 
 Token lex_next_token() {
 	char str[32];
 	int str_counter = 0;
-	
-	char ch = NEXT();
+
+	char ch = PEEK();
 	/* check if the current character is EOF */
 	if (ch == EOF) { 
 		return t_init_token(str, EOF_T);
@@ -37,27 +34,41 @@ Token lex_next_token() {
 
 	/* do the actual lexing stuff here */
 	char tag;
-	if (isalpha(ch) || ch == '_' || ch == '-') {
+	if (isalpha(ch) || ch == '_' || ch == '-') { /* check if it's a variable */
 		while (isalnum(ch) || ch == '_' || ch == '-') {	
 			str[str_counter] = ch;
 			str_counter++;
 			ch = NEXT();
 		}		
 		tag = VARIABLE_T;
-	} else {
+	} else if (isdigit(ch)) { /*if it's a number*/
+		bool is_float = false;
+		while (isdigit(ch)) {	
+			str[str_counter] = ch;
+			str_counter++;
+			ch = NEXT();
+			if(ch == '.' && is_float == false) {/* check if its a float */
+				str[str_counter] = ch;
+				str_counter++;
+				ch = NEXT();
+				is_float = true;
+			}
+		}	
+		if(is_float) {
+			tag = FLOAT_T;	
+		} else {
+			tag = INT_T;
+		}
+	} else { /*else, error*/
 		print_error("Invalid token");
 		exit(1);
-	}
-	str[str_counter] = '\0';
+	} 
 	
+	str[str_counter] = '\0';	
+
 	/* skip whitespaces */
-	while (ch != EOF) {	
-		ch = PEEK();
-		if (ch == ' ' || ch == '\t' || ch == '\n') {
-			NEXT();
-		} else {
-			break;
-		}
+	while (ch == ' ' || ch == '\t' || ch == '\n') {	
+		ch = NEXT();
 	}
 
 	return t_init_token(str, tag);
