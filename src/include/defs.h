29a09adc9b5ea37f2cd8b9d716daed9c8304e8a1
blend-smile <blend-smile@protonmail.com>
2021-09-26 10:38:52 +0700

    change defs to enum

--- src/include/defs.h
+++ src/include/defs.h
@@ -1,52 +1,55 @@
 /* TAG */
-#define EOF_T		0
-#define EOL_T		1
-#define VARIABLE_T 	2
-#define INT_T 		3
-#define FLOAT_T 	4
-#define STRING_T	5
-#define OPERATOR_T 	6
-#define SYMBOL_T	7
-#define FOR_T		8
-#define WHILE_T		9
-#define FN_T		10
-#define RETURN_T	11
-#define IMPORT_T	12
-#define IF_T		13
-#define ELIF_T		14
-#define ELSE_T		15
-#define BREAK_T		16
-#define CONTINUE_T	17
-
+enum tags{
+ EOF_T,
+ EOL_T,
+ VARIABLE_T ,
+ INT_T ,
+ FLOAT_T ,
+ STRING_T,
+ OPERATOR_T ,
+ SYMBOL_T,
+ FOR_T,
+ WHILE_T,
+ FN_T,
+ RETURN_T,
+ IMPORT_T,
+ IF_T,
+ ELIF_T,
+ ELSE_T,
+ BREAK_T,
+ CONTINUE_T,
+}
 /* TAG2 */
-#define ADD_OPR_T 18
-#define SUB_OPR_T 19
-#define MUL_OPR_T 20
-#define DIV_OPR_T 21
-#define MOD_OPR_T 22
-#define AND_OPR_T 23
-#define OR_OPR_T 24
-#define XOR_OPR_T 25
-#define CGREATER_OPR_T 26
-#define CLESS_OPR_T 27
-#define ASSIGNMENT_OPR_T 28
-#define INC_OPR_T 29
-#define DEC_OPR_T 30
-#define MUL_ASSIGN_OPR_T 31
-#define DIV_ASSIGN_OPR_T 32
-#define MOD_ASSIGN_OPR_T 33
-#define AND_ASSIGN_OPR_T 34
-#define OR_ASSIGN_OPR_T 35
-#define XOR_ASSIGN_OPR_T 36
-#define CGREATER_EQU_OPR_T 37
-#define CLESS_EQU_OPR_T 38
-#define CEQU_OPR_T 39
-#define LAND_OPR_T 40
-#define LOR_OPR_T 41
-#define SHR_OPR_T 42
-#define SHL_OPR_T 43
-#define SHR_ASSIGN_OPR_T 44
-#define SHL_ASSIGN_OPR_T 45
+enum tags2{
+ ADD_OPR_T,
+ SUB_OPR_T,
+ MUL_OPR_T,
+ DIV_OPR_T,
+ MOD_OPR_T,
+ AND_OPR_T,
+ OR_OPR_T,
+ XOR_OPR_T,
+ CGREATER_OPR_T,
+ CLESS_OPR_T,
+ ASSIGNMENT_OPR_T,
+ INC_OPR_T,
+ DEC_OPR_T,
+ MUL_ASSIGN_OPR_T,
+ DIV_ASSIGN_OPR_T,
+ MOD_ASSIGN_OPR_T,
+ AND_ASSIGN_OPR_T,
+ OR_ASSIGN_OPR_T,
+ XOR_ASSIGN_OPR_T,
+ CGREATER_EQU_OPR_T,
+ CLESS_EQU_OPR_T,
+ CEQU_OPR_T,
+ LAND_OPR_T,
+ LOR_OPR_T,
+ SHR_OPR_T,
+ SHL_OPR_T,
+ SHR_ASSIGN_OPR_T,
+ SHL_ASSIGN_OPR_T,
+}
 
 #define MAX_TOKEN_VALUE 50
 #define INIT_DSTR_SPACE 2048
