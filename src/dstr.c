c7a8d03e53e7f4b2f6b301135c19aa09fc9d40f8
blend-smile <blend-smile@protonmail.com>
2021-11-02 21:33:39 +0700

    fix memory bugs/errors

--- src/dstr.c
+++ src/dstr.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stddef.h>
@@ -6,18 +7,6 @@
 #include "include/types.h"
 #include "include/compiler.h"
 
-/* the following function creates a new token from the given arguments */
-token_t t_init_token(char *value, tag_t tag, tag_t tag2) {
-	token_t token; 
-	int i = 0;
-	while (*(value+i) != '\0' || i < MAX_TOKEN_VALUE) {
-		token.value[i] = *(value+i);
-		i++;
-	}
-	token.tag = tag;
-	token.tag2 = tag2;
-	return token;
-}	
 
 /* like strcpy but doesn't copy the \0 (stracpy = str_append_cpy).
  * Returns char* pointing to the last character copied. */
@@ -33,6 +22,7 @@ char* stracpy(char* source, char* dest) {
 void dstr_init(dstr_t* dstr) {
 	dstr->str = malloc(INIT_DSTR_SPACE);
 	dstr->end = dstr->str;
+	dstr->alloc_end = dstr->str + INIT_DSTR_SPACE;
 }
 
 void dstr_erase(dstr_t* dstr) {
@@ -49,11 +39,30 @@ void dstr_free(dstr_t* dstr) {
 	free(dstr->str);
 }
 
+static inline void dstr_double_size(dstr_t* dest) {
+	char* old_str = dest->str;
+	int new_size = (dest->alloc_end + INIT_DSTR_SPACE) - dest->str;
+	if ((dest->str = realloc(dest->str, new_size)) == NULL) {
+		printf("FATAL: failed to allocate memory for dstr\n");
+		exit(1);
+	}
+	dest->alloc_end = dest->str + new_size;
+	dest->end = dest->str + (dest->end - old_str);
+}
+
 void dstr_append(dstr_t* dest, char* str) {
+	/* realloc if the capacity is not enough */
+	if (dest->end + strlen(str) >= dest->alloc_end) {
+		dstr_double_size(dest);
+	}
 	dest->end = stracpy(str, dest->end)+1;
 }
 
 void dstr_append_char(dstr_t* dest, char ch) {
+	/* realloc if the capacity is not enough */
+	if (dest->end + 1 >= dest->alloc_end) {
+		dstr_double_size(dest);
+	}
 	*(dest->end) = ch;
 	dest->end++;
 }
