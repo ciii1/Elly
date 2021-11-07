3f28953e9032c171cc030bfd309e08d19d02bef5
blend-smile <blend-smile@protonmail.com>
2021-11-07 09:40:33 +0700

    fix bug by expanding the string by the size of the string source on dstr_append \n and by expanding it twice the size on dstr_append_char

--- src/dstr.c
+++ src/dstr.c
@@ -25,9 +25,9 @@ void dstr_init(dstr_t* dstr) {
 	dstr->alloc_end = dstr->str + INIT_DSTR_SPACE;
 }
 
-static inline void dstr_double_size(dstr_t* dest) {
+void dstr_expand(dstr_t* dest, int size) {
 	char* old_str = dest->str;
-	int new_size = (dest->alloc_end + INIT_DSTR_SPACE) - dest->str;
+	int new_size = dest->end - dest->str + size;
 	if ((dest->str = realloc(dest->str, new_size)) == NULL) {
 		printf("FATAL: failed to allocate memory for dstr\n");
 		exit(1);
@@ -38,8 +38,9 @@ static inline void dstr_double_size(dstr_t* dest) {
 
 void dstr_append(dstr_t* dest, char* str) {
 	/* realloc if the capacity is not enough */
-	if (dest->end + strlen(str) >= dest->alloc_end) {
-		dstr_double_size(dest);
+	int str_len = strlen(str);
+	if (dest->end + str_len >= dest->alloc_end) {
+		dstr_expand(dest, str_len+1);
 	}
 	dest->end = stracpy(str, dest->end)+1;
 }
@@ -47,7 +48,7 @@ void dstr_append(dstr_t* dest, char* str) {
 void dstr_append_char(dstr_t* dest, char ch) {
 	/* realloc if the capacity is not enough */
 	if (dest->end + 1 >= dest->alloc_end) {
-		dstr_double_size(dest);
+		dstr_expand(dest, INIT_DSTR_SPACE);
 	}
 	*(dest->end) = ch;
 	dest->end++;
